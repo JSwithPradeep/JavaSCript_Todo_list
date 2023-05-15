c194d094dcf4cc462866ea261c70c2888cfc5835
Pradeep <psrana055@gmail.com>
2023-05-14 23:45:38 -0700

    TodoList

---
+++ TodoList/index.js
@@ -0,0 +1,9 @@
+const item = document.querySelector('#item');
+const todoBox = document.querySelector('#to-do');
+
+item.addEventListener (
+    "keyup",
+    function (event) {
+        console.log(event)
+    }
+)
